b206dbe1d1863a699820044579fe31fcfa906d45
alecthibodeau <alecthibodeau@gmail.com>
2018-09-09 11:18:40 -0400

    Rename Heroku app in config file.

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: 'https://nameless-oasis-50311.herokuapp.com',
+  production: 'https://radtabs-api.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
