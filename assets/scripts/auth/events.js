a949108818b6f4f84066b0126c18cc20cb3071ac
alecthibodeau <alecthibodeau@gmail.com>
2021-02-20 06:16:31 -0500

    Add password validation for 8 characters.

--- assets/scripts/auth/events.js
+++ assets/scripts/auth/events.js
@@ -5,8 +5,28 @@ const getFormFields = require('../../../lib/get-form-fields')
 const api = require('./api')
 const ui = require('./ui')
 
+const validatePassword = (form) => {
+  console.log('validatePassword runs');
+  const elements = form.elements || []
+  for (let i = 0; i < elements.length; i++) {
+    const e = elements[i]
+    if (e.hasAttribute('name')) {
+      if ((e.getAttribute('name') === 'credentials[password]' || e.getAttribute('name') === 'passwords[new]')) {
+        // Password must be a minimum of 8 characters long, containing at least one letter and one number.
+        const passwordValidation = new RegExp(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/);
+        const validPassword = passwordValidation.test(e.value);
+        void (!validPassword && $('.password-validation-message').addClass('validation-error'));
+        return validPassword;
+      }
+    }
+  }
+}
+
 const onSignUp = function (event) {
   event.preventDefault()
+  if (!validatePassword(this)) {
+    return;
+  }
   $('#modalTitleSignUp').text('Signing up…')
   const data = getFormFields(this)
   api.signUp(data)
@@ -16,6 +36,9 @@ const onSignUp = function (event) {
 
 const onSignIn = function (event) {
   event.preventDefault()
+  if (!validatePassword(this)) {
+    return;
+  }
   $('#modalTitleSignIn').text('Signing in…')
   const data = getFormFields(this)
   api.signIn(data)
@@ -25,7 +48,9 @@ const onSignIn = function (event) {
 
 const onChangePassword = function (event) {
   event.preventDefault()
-
+  if (!validatePassword(this)) {
+    return;
+  }
   const data = getFormFields(this)
 
   api.changePassword(data)
@@ -41,11 +66,16 @@ const onSignOut = function (event) {
     .catch(ui.signOutFailure)
 }
 
+const removeValidationMessage = () => {
+  $('.password-validation-message').removeClass('validation-error');
+}
+
 const addAuthHandlers = () => {
   $('#sign-up').on('submit', onSignUp)
   $('#sign-in').on('submit', onSignIn)
   $('#change-password').on('submit', onChangePassword)
   $('#nav-sign-out').on('click', onSignOut)
+  $('.validation-related-button').on('click', removeValidationMessage);
 }
 
 module.exports = {
